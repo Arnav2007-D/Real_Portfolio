f9cdcfdf2f8eeec0d8b1f58b1683318528b65997
Arnav2007-D <arnav.payal77@gmail.com>
2025-12-05 12:38:46 +0800

    Add files via upload

---
+++ ShoppingCart.py
@@ -0,0 +1,29 @@
+#Shopping Cart Program: To help you keep count of the
+# total money your spending on your grocery items.
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    print("1) Add item")
+    print("2) Show cart & Exit")
+    choice = int(input("Enter your choice: "))
+    
+    if choice == 2:
+        break
+    
+    item = input("Enter a Grocery Item (q to quit): ")
+    if item.lower() == "q":
+        continue
+        
+    price = float(input(f"Enter the Price of {item}: $"))
+    foods.append(item)
+    prices.append(price)
+
+# Final cart display (runs once at end)
+print("--------CART TOTAL-------")
+for item, price in zip(foods, prices):
+    print(f"{item}: ${price}")
+total = sum(prices)
+print(f"Your total is ${total}")
